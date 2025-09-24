76480a23d44e599936f1b294331c3fb1d0e86e6a
unkabogaton <anthonralph2@gmail.com>
2025-09-24 16:07:48 +0800

    fix: strov import error

--- internal/infrastructure/grpc/server.go
+++ internal/infrastructure/grpc/server.go
@@ -1,104 +1,104 @@
 package grpc
 
 import (
-    "context"
-    "net"
+	"context"
+	"net"
 
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/reflection"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
-    "github.com/unkabogaton/github-users/internal/domain/interfaces"
-    entities "github.com/unkabogaton/github-users/internal/domain/entities"
-    derr "github.com/unkabogaton/github-users/internal/domain/errors"
-    gen "github.com/unkabogaton/github-users/internal/infrastructure/grpc/gen"
+	entities "github.com/unkabogaton/github-users/internal/domain/entities"
+	derr "github.com/unkabogaton/github-users/internal/domain/errors"
+	"github.com/unkabogaton/github-users/internal/domain/interfaces"
+	gen "github.com/unkabogaton/github-users/internal/infrastructure/grpc/gen"
 )
 
 type Server struct {
-    gen.UnimplementedUserServiceServer
-    userService interfaces.UserService
+	gen.UnimplementedUserServiceServer
+	userService interfaces.UserService
 }
 
 func NewServer(userService interfaces.UserService) *Server {
-    return &Server{userService: userService}
+	return &Server{userService: userService}
 }
 
 func (server *Server) ListenAndServe(address string) error {
-    tcpListener, err := net.Listen("tcp", address)
-    if err != nil {
-        return err
-    }
-    grpcServerInstance := grpc.NewServer()
-    gen.RegisterUserServiceServer(grpcServerInstance, server)
-    reflection.Register(grpcServerInstance)
-    return grpcServerInstance.Serve(tcpListener)
+	tcpListener, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+	grpcServerInstance := grpc.NewServer()
+	gen.RegisterUserServiceServer(grpcServerInstance, server)
+	reflection.Register(grpcServerInstance)
+	return grpcServerInstance.Serve(tcpListener)
 }
 
 func mapUserEntityToProto(userEntity *entities.User) *gen.User {
-    return &gen.User{
-        Id:           int64(userEntity.ID),
-        Login:        userEntity.Login,
-        NodeId:       userEntity.NodeID,
-        AvatarUrl:    userEntity.AvatarURL,
-        Url:          userEntity.URL,
-        HtmlUrl:      userEntity.HTMLURL,
-        Type:         userEntity.Type,
-        UserViewType: userEntity.UserViewType,
-        SiteAdmin:    userEntity.SiteAdmin,
-    }
+	return &gen.User{
+		Id:           int64(userEntity.ID),
+		Login:        userEntity.Login,
+		NodeId:       userEntity.NodeID,
+		AvatarUrl:    userEntity.AvatarURL,
+		Url:          userEntity.URL,
+		HtmlUrl:      userEntity.HTMLURL,
+		Type:         userEntity.Type,
+		UserViewType: userEntity.UserViewType,
+		SiteAdmin:    userEntity.SiteAdmin,
+	}
 }
 
 func (server *Server) ListUsers(ctx context.Context, _ *gen.Empty) (*gen.UserList, error) {
-    userEntities, err := server.userService.List(ctx)
-    if err != nil {
-        return nil, err
-    }
+	userEntities, err := server.userService.List(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-    userListResponse := &gen.UserList{}
-    for i := range userEntities {
-        userListResponse.Users = append(userListResponse.Users, mapUserEntityToProto(&userEntities[i]))
-    }
+	userListResponse := &gen.UserList{}
+	for i := range userEntities {
+		userListResponse.Users = append(userListResponse.Users, mapUserEntityToProto(&userEntities[i]))
+	}
 
-    return userListResponse, nil
+	return userListResponse, nil
 }
 
 func (server *Server) GetUser(ctx context.Context, request *gen.GetUserRequest) (*gen.User, error) {
-    username := request.GetUsername()
-    userEntity, err := server.userService.Get(ctx, username)
-    if err != nil {
-        return nil, err
-    }
-    return mapUserEntityToProto(userEntity), nil
+	username := request.GetUsername()
+	userEntity, err := server.userService.Get(ctx, username)
+	if err != nil {
+		return nil, err
+	}
+	return mapUserEntityToProto(userEntity), nil
 }
 
 func (server *Server) UpdateUser(ctx context.Context, request *gen.UpdateUserRequest) (*gen.User, error) {
-    updateRequest := interfaces.UpdateUserRequest{
-        Login:        request.GetLogin(),
-        NodeID:       request.GetNodeId(),
-        AvatarURL:    request.GetAvatarUrl(),
-        URL:          request.GetUrl(),
-        HTMLURL:      request.GetHtmlUrl(),
-        Type:         request.GetType(),
-        UserViewType: request.GetUserViewType(),
-        SiteAdmin:    request.GetSiteAdmin(),
-    }
+	updateRequest := interfaces.UpdateUserRequest{
+		Login:        request.GetLogin(),
+		NodeID:       request.GetNodeId(),
+		AvatarURL:    request.GetAvatarUrl(),
+		URL:          request.GetUrl(),
+		HTMLURL:      request.GetHtmlUrl(),
+		Type:         request.GetType(),
+		UserViewType: request.GetUserViewType(),
+		SiteAdmin:    request.GetSiteAdmin(),
+	}
 
-    updatedUser, err := server.userService.Update(ctx, request.GetUsername(), updateRequest)
-    if err != nil {
-        return nil, err
-    }
+	updatedUser, err := server.userService.Update(ctx, request.GetUsername(), updateRequest)
+	if err != nil {
+		return nil, err
+	}
 
-    return mapUserEntityToProto(updatedUser), nil
+	return mapUserEntityToProto(updatedUser), nil
 }
 
 func (server *Server) DeleteUser(ctx context.Context, request *gen.DeleteUserRequest) (*gen.DeleteUserResponse, error) {
-    username := request.GetUsername()
-    if username == "" {
-        return nil, derr.New(derr.ErrorCodeValidation, "username is required")
-    }
+	username := request.GetUsername()
+	if username == "" {
+		return nil, derr.New(derr.ErrorCodeValidation, "username is required")
+	}
 
-    if err := server.userService.Delete(ctx, username); err != nil {
-        return nil, err
-    }
+	if err := server.userService.Delete(ctx, username); err != nil {
+		return nil, err
+	}
 
-    return &gen.DeleteUserResponse{Username: username}, nil
+	return &gen.DeleteUserResponse{Username: username}, nil
 }
--- internal/infrastructure/http/controllers/user_controller.go
+++ internal/infrastructure/http/controllers/user_controller.go
@@ -1,12 +1,13 @@
 package controllers
 
 import (
-    "net/http"
+	"net/http"
+	"strconv"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 
-    derr "github.com/unkabogaton/github-users/internal/domain/errors"
-    "github.com/unkabogaton/github-users/internal/domain/interfaces"
+	derr "github.com/unkabogaton/github-users/internal/domain/errors"
+	"github.com/unkabogaton/github-users/internal/domain/interfaces"
 )
 
 type UserController struct {
@@ -18,41 +19,40 @@ func NewUserController(userService interfaces.UserService) *UserController {
 }
 
 func (controller *UserController) ListUsers(ginContext *gin.Context) {
-    httpRequestContext := ginContext.Request.Context()
+	httpRequestContext := ginContext.Request.Context()
 
-    defaultLimit := 10
-    defaultPage := 1
-    sortColumn := ginContext.DefaultQuery("orderby", "id")
-    sortDirection := ginContext.DefaultQuery("order", "asc")
+	defaultLimit := 10
+	defaultPage := 1
+	sortColumn := ginContext.DefaultQuery("orderby", "id")
+	sortDirection := ginContext.DefaultQuery("order", "asc")
 
-    if limitQueryValue := ginContext.DefaultQuery("limit", ""); limitQueryValue != "" {
-        if parsedLimit, parseError := strconv.Atoi(limitQueryValue); parseError == nil {
-            defaultLimit = parsedLimit
-        }
-    }
+	if limitQueryValue := ginContext.DefaultQuery("limit", ""); limitQueryValue != "" {
+		if parsedLimit, parseError := strconv.Atoi(limitQueryValue); parseError == nil {
+			defaultLimit = parsedLimit
+		}
+	}
 
-    if pageQueryValue := ginContext.DefaultQuery("page", ""); pageQueryValue != "" {
-        if parsedPage, parseError := strconv.Atoi(pageQueryValue); parseError == nil {
-            defaultPage = parsedPage
-        }
-    }
+	if pageQueryValue := ginContext.DefaultQuery("page", ""); pageQueryValue != "" {
+		if parsedPage, parseError := strconv.Atoi(pageQueryValue); parseError == nil {
+			defaultPage = parsedPage
+		}
+	}
 
-    listOptions := interfaces.ListOptions{
-        Limit:          defaultLimit,
-        Page:           defaultPage,
-        OrderBy:        sortColumn,
-        OrderDirection: sortDirection,
-    }
+	listOptions := interfaces.ListOptions{
+		Limit:          defaultLimit,
+		Page:           defaultPage,
+		OrderBy:        sortColumn,
+		OrderDirection: sortDirection,
+	}
 
-    users, userListError := controller.userService.List(httpRequestContext, listOptions)
-    if userListError != nil {
-        _ = ginContext.Error(userListError)
-        return
-    }
+	users, userListError := controller.userService.List(httpRequestContext, listOptions)
+	if userListError != nil {
+		_ = ginContext.Error(userListError)
+		return
+	}
 
-    ginContext.JSON(http.StatusOK, users)
+	ginContext.JSON(http.StatusOK, users)
 }
-
 
 func (c *UserController) GetUser(ctx *gin.Context) {
 	requestContext := ctx.Request.Context()
@@ -60,12 +60,11 @@ func (c *UserController) GetUser(ctx *gin.Context) {
 
 	user, err := c.userService.Get(requestContext, username)
 	if err != nil {
-        // If service did not categorize, treat not found sensibly at controller level
-        if derr.IsCode(err, derr.ErrorCodeNotFound) {
-            _ = ctx.Error(err)
-        } else {
-            _ = ctx.Error(derr.Wrap(derr.ErrorCodeInternal, "failed to get user", err))
-        }
+		if derr.IsCode(err, derr.ErrorCodeNotFound) {
+			_ = ctx.Error(err)
+		} else {
+			_ = ctx.Error(derr.Wrap(derr.ErrorCodeInternal, "failed to get user", err))
+		}
 		return
 	}
 
@@ -77,7 +76,7 @@ func (c *UserController) UpdateUser(ctx *gin.Context) {
 
 	var updateRequest interfaces.UpdateUserRequest
 	if err := ctx.ShouldBindJSON(&updateRequest); err != nil {
-        _ = ctx.Error(derr.Wrap(derr.ErrorCodeValidation, "invalid request payload", err))
+		_ = ctx.Error(derr.Wrap(derr.ErrorCodeValidation, "invalid request payload", err))
 		return
 	}
 
@@ -87,7 +86,7 @@ func (c *UserController) UpdateUser(ctx *gin.Context) {
 		updateRequest,
 	)
 	if err != nil {
-        _ = ctx.Error(derr.Wrap(derr.ErrorCodeInternal, "failed to update user", err))
+		_ = ctx.Error(derr.Wrap(derr.ErrorCodeInternal, "failed to update user", err))
 		return
 	}
 
@@ -99,9 +98,9 @@ func (c *UserController) DeleteUser(ctx *gin.Context) {
 	requestContext := ctx.Request.Context()
 
 	if deleteError := c.userService.Delete(requestContext, username); deleteError != nil {
-        _ = ctx.Error(derr.Wrap(derr.ErrorCodeInternal, "failed to delete user", deleteError))
+		_ = ctx.Error(derr.Wrap(derr.ErrorCodeInternal, "failed to delete user", deleteError))
 		return
 	}
 
-    ctx.JSON(http.StatusOK, gin.H{"username": username})
+	ctx.JSON(http.StatusOK, gin.H{"username": username})
 }
